e6e3bff49cbaa5ff8f188ac04023ffa0eca925b4
Evaldas Pocius <evaldukas96@gmail.com>
2024-12-02 21:28:14 +0200

    2024 day 1 part 2

--- 2024/day-01/day-01.js
+++ 2024/day-01/day-01.js
@@ -6,10 +6,10 @@ const input = fs.readFileSync("input.txt", "utf8");
 const [list1, list2] = prepareLists(input);
 const length = list1.length;
 
-let totalDistance = 0;
-
 list1.sort((a, b) => a - b);
 list2.sort((a, b) => a - b);
+
+let totalDistance = 0;
 
 for (let index = 0; index < length; index++) {
   const distance = Math.abs(list1[index] - list2[index]);
@@ -18,6 +18,26 @@ for (let index = 0; index < length; index++) {
 }
 
 console.log("Total Distance:", totalDistance);
+
+// --- Part Two ---
+
+let similarityScore = 0;
+
+for (let index = 0; index < length; index++) {
+  const firstListId = list1[index];
+
+  const secondListOccurrences = list2.filter(
+    (id) => id === firstListId
+  )?.length;
+
+  if (!secondListOccurrences) {
+    continue;
+  }
+
+  similarityScore += firstListId * secondListOccurrences;
+}
+
+console.log("Similarity Score:", similarityScore);
 
 function prepareLists(data) {
   const lines = data.split("\n");
