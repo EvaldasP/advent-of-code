90646ff37495bd290c098cb5adfeadba3880f5b9
Evaldas Pocius <evaldukas96@gmail.com>
2024-12-03 16:35:26 +0200

    2024 day 2 part 2

--- 2024/day-02/day-02.js
+++ 2024/day-02/day-02.js
@@ -7,33 +7,63 @@ const reports = prepareReports(input);
 let saveReports = 0;
 
 for (const report of reports) {
-  const isReportIncreasing = report[0] < report[1];
-  let isReportSave = true;
-
-  for (let index = 0; index < report.length; index++) {
-    const level = report[index];
-    const nextLevel = report[index + 1];
-
-    if (
-      isReportIncreasing &&
-      (level >= nextLevel || nextLevel - level > 3 || nextLevel - level > 3)
-    ) {
-      isReportSave = false;
-      break;
-    }
-
-    if (!isReportIncreasing && (level <= nextLevel || level - nextLevel > 3)) {
-      isReportSave = false;
-      break;
-    }
-  }
-
-  if (isReportSave) {
+  if (checkChangingSafely(report)) {
     saveReports++;
   }
 }
 
 console.log("Save Reports:", saveReports);
+
+// --- Part Two ---
+let safeReportsCount = 0;
+
+for (const report of reports) {
+  let result = checkChangingSafely(report);
+
+  // If the report is safe already
+  if (result) {
+    safeReportsCount++;
+    continue;
+  }
+
+  let isReportSafeWithDampener = false;
+
+  for (let index = 0; index < report.length; index++) {
+    const newReport = [...report];
+    newReport.splice(index, 1);
+
+    result = checkChangingSafely(newReport);
+
+    if (result) {
+      isReportSafeWithDampener = true;
+      break;
+    }
+  }
+
+  if (isReportSafeWithDampener) {
+    safeReportsCount++;
+  }
+}
+
+console.log("Save Reports Dampened:", safeReportsCount);
+
+function checkChangingSafely(report) {
+  const isReportIncreasing = report[0] < report[1];
+
+  for (let index = 0; index < report.length - 1; index++) {
+    const level = report[index];
+    const nextLevel = report[index + 1];
+
+    if (
+      (isReportIncreasing && (level >= nextLevel || nextLevel - level > 3)) ||
+      (!isReportIncreasing && (level <= nextLevel || level - nextLevel > 3))
+    ) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 function prepareReports(data) {
   const lines = data.split("\n");
