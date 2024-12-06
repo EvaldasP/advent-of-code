f6ecd7d152c8d1cd5cb5962023f7fb1084421875
Evaldas Pocius <evaldukas96@gmail.com>
2024-12-06 20:32:59 +0200

    2024 day 4 part 1

---
+++ 2024/day-04/day-04.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const text = fs.readFileSync("input.txt", "utf8");
+
+// --- Part One ---
+
+let xmasCount = 0;
+
+const letterMap = text.split("\n").map((string) => [...string]);
+const directions = [
+  { dr: -1, dc: 0 }, // Top
+  { dr: -1, dc: 1 }, // Top-Right
+  { dr: 0, dc: 1 }, // Right
+  { dr: 1, dc: 1 }, // Bottom-Right
+  { dr: 1, dc: 0 }, // Bottom
+  { dr: 1, dc: -1 }, // Bottom-Left
+  { dr: 0, dc: -1 }, // Left
+  { dr: -1, dc: -1 }, // Top-Left
+];
+
+for (let row = 0; row < letterMap.length; row++) {
+  for (let col = 0; col < letterMap[row].length; col++) {
+    if (letterMap[row][col] === "X") {
+      for (const { dr, dc } of directions) {
+        if (checkWord(row, col, dr, dc)) {
+          xmasCount++;
+        }
+      }
+    }
+  }
+}
+
+console.log(xmasCount);
+
+function checkWord(row, col, dr, dc) {
+  const m = letterMap[row + dr]?.[col + dc];
+  const a = letterMap[row + 2 * dr]?.[col + 2 * dc];
+  const s = letterMap[row + 3 * dr]?.[col + 3 * dc];
+  return m === "M" && a === "A" && s === "S";
+}
