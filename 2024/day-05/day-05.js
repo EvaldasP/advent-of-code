523d39a3bd480b7a52b687531d0bbf9f2a40253a
Evaldas Pocius <evaldukas96@gmail.com>
2024-12-07 15:58:16 +0200

    2024 day 5 part 2

--- 2024/day-05/day-05.js
+++ 2024/day-05/day-05.js
@@ -3,21 +3,17 @@ const data = fs.readFileSync("input.txt", "utf8");
 
 const { rules, updates } = prepareData(data);
 let correctUpdates = [];
+let fixedUpdates = [];
 
 for (const update of updates) {
   let isCorrectUpdate = true;
 
   for (const rule of rules) {
-    const [first, second] = rule.split("|");
-
-    if (![first, second].every((page) => update.includes(page))) {
+    if (ignoreRule(rule, update)) {
       continue;
     }
 
-    const indexFirst = update.indexOf(first);
-    const indexSecond = update.indexOf(second);
-
-    if (indexFirst < indexSecond) {
+    if (validateRule(rule, update)) {
       isCorrectUpdate = true;
     } else {
       isCorrectUpdate = false;
@@ -27,12 +23,67 @@ for (const update of updates) {
 
   if (isCorrectUpdate) {
     correctUpdates.push(update);
+    continue;
   }
+
+  fixedUpdates.push(fixUpdate(update, rules));
 }
 
 // --- Part One ---
-
 console.log("Total Of Middle Pages:", countMiddlePageTotal(correctUpdates));
+
+// --- Part Two ---
+console.log(
+  "Total Of Middle Pages Fixed Updates:",
+  countMiddlePageTotal(fixedUpdates)
+);
+
+function fixUpdate(update, rules) {
+  let modifiedUpdate = update.split(",");
+  let flagged = true;
+
+  while (flagged) {
+    flagged = false;
+    for (const rule of rules) {
+      if (ignoreRule(rule, modifiedUpdate)) {
+        continue;
+      }
+
+      if (!validateRule(rule, modifiedUpdate)) {
+        const [first, second] = rule.split("|");
+
+        const indexFirst = modifiedUpdate.indexOf(first);
+        const indexSecond = modifiedUpdate.indexOf(second);
+
+        // swap
+        modifiedUpdate[indexFirst] = second;
+        modifiedUpdate[indexSecond] = first;
+        flagged = true;
+        break;
+      }
+    }
+  }
+
+  return modifiedUpdate.join();
+}
+
+function validateRule(rule, update) {
+  const [first, second] = rule.split("|");
+
+  const indexFirst = update.indexOf(first);
+  const indexSecond = update.indexOf(second);
+
+  if (indexFirst < indexSecond) {
+    return true;
+  }
+
+  return false;
+}
+
+function ignoreRule(rule, update) {
+  const [first, second] = rule.split("|");
+  return ![first, second].every((page) => update.includes(page));
+}
 
 function countMiddlePageTotal(correctUpdates) {
   return correctUpdates.reduce((acc, update) => {
